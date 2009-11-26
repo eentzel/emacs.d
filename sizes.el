7fa701f9b74f23cda271b625856a5cf5db7f1519
Eric Entzel <eric@ubermac.net>
2009-11-26 00:15:12 +0000

    Function/keybind to toggle between common frame sizes

---
+++ sizes.el
@@ -0,0 +1,12 @@
+(setq sizes-alist '((232 . 62) (100 . 50))
+      sizes-list-pos 0)
+
+(defun next-size ()
+  "move to the next window size in 'sizes-alist"
+  (interactive)
+  (setq sizes-list-pos (% (1+ sizes-list-pos) (length sizes-alist)))
+  (let ((size (nth sizes-list-pos sizes-alist)))
+    (set-frame-width (selected-frame) (car size))
+    (set-frame-height (selected-frame) (cdr size))))
+
+(global-set-key "\C-cs" 'next-size)
