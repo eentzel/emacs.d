e2bf31a3165b7682c5936efc0d34e544cc900f56
Eric Entzel <eric@ubermac.net>
2010-03-03 12:02:34 -0700

    Found a way to make mark-whole-word activate transient-mark-mode only temporarily

--- mark-whole-word.el
+++ mark-whole-word.el
@@ -1,6 +1,9 @@
 (defun mark-whole-word ()
   (interactive)
-  (transient-mark-mode 1)
+  (if (and mark-active (null transient-mark-mode))
+      ; setting transient-mark-mode to 'lambda instead of t means it
+      ; will be deactivated by the next set-mark-command
+      (setq transient-mark-mode 'lambda))
   (skip-syntax-backward "\w")
   (mark-word))
 (global-set-key (kbd "M-SPC") 'mark-whole-word)
