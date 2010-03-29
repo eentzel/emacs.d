c287a05be74196cf9934518575d23728a797af10
Eric Entzel <eric@ubermac.net>
2010-03-29 16:28:35 -0600

    configure ffap for JS/CSS files

--- ffap-epe.el
+++ ffap-epe.el
@@ -2,4 +2,6 @@
 (defun ffap-lmi-jsp (file)
  (concat (guess-lmi-project-root buffer-file-name) "/src/main/webapp" file))
 (add-to-list 'ffap-alist '("WEB-INF" . ffap-lmi-jsp))
+(add-to-list 'ffap-alist '("/js" . ffap-lmi-jsp))
+(add-to-list 'ffap-alist '("/css" . ffap-lmi-jsp))
 (global-set-key "\C-cf" 'ffap)
