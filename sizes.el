61c577692fa8937a98d15a465c1d70d8bada8c0f
Eric Entzel <eric@ubermac.net>
2010-06-23 14:10:39 -0600

    Add keybind to move frame to the edge of the screen, alternating between accounting for the Dock and not

--- sizes.el
+++ sizes.el
@@ -1,5 +1,7 @@
 (setq sizes-alist '((232 . 62) (175 . 46))
-      sizes-list-pos 0)
+      sizes-list-pos 0
+      positions-alist '((8 . 26) (75 . 26))
+      positions-list-pos 0)
 
 (defun next-size ()
   "move to the next window size in 'sizes-alist"
@@ -10,3 +12,11 @@
     (set-frame-height (selected-frame) (cdr size))))
 
 (global-set-key "\C-cs" 'next-size)
+
+(defun move-frame-to-screen-edge ()
+  (interactive)
+  (setq positions-list-pos (% (1+ positions-list-pos) (length positions-alist)))
+  (let ((pos (nth positions-list-pos positions-alist)))
+    (set-frame-position (selected-frame) (car pos) (cdr pos))))
+
+(global-set-key "\C-ce" 'move-frame-to-screen-edge)
