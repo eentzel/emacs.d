d62b190191db8ce35c04dc967780c67bcaf64b49
Eric Entzel <eric@ubermac.net>
2010-08-12 13:35:13 -0600

    Refactor syncer to 1) use guess-lmi-project-name, and 2) sync name->name if it's not found in the mapping alist

--- syncer.el
+++ syncer.el
@@ -1,34 +1,22 @@
-; TODO - refactor to use guess-lmi-project-root
-
 ; Requirements:
-;     environment variable $WORKSPACE must be set
 ;     syncer must be in your $PATH
 
-(setq projects-alist '( ("ednl" ."ednl")
-                        ("edpl" . "edpl")
-                        ("eddk" ."eddk")
-                        ("edcz" . "edcz")
-                        ("edtas" . "edtas")
-                        ("ed-core-web" . "edtas")
+(setq project-mapping '(("ed-core-web" . "ednl")
+                        ("ed-core-web-4.1" . "ednl")
                         ("ed-core-web-3.0" . "ednl")
                         ("ed-core-web-3.3" . "eddk")
                         ("ed-tas-core-web-2.1" . "edpl")
-                        ("ds-core-web" . "edtas")
-                        ("wdbe" . "wdbe")
-                        ("edfi" . "edfi")
-                        ("wdnl" . "wdnl")
-                        ("mris" . "mris")
-                        ("edse" . "edse")))
-(setq workspace (getenv "WORKSPACE"))
+                        ("ds-core-web" . "ednl")
+                        ("ds-core-web-4.4" . "ednl")))
 
 (defun syncer ()
   "Call syncer to sync changes to Tomcat if appropriate."
   (when buffer-file-name
-    (dolist (item projects-alist)
-      (let ((name (car item)) (target (cdr item)))
-        (when (string-match (concat workspace "/" name "/") buffer-file-name)
-          (if (eq (call-process "syncer2" () () () target) 0)
-              (message (concat "Synced " name " and " (current-message)))
-            (message (concat (current-message) " - couldn't sync " name))))))))
+    (let (name target)
+      (setq name (guess-lmi-project-name buffer-file-name))
+      (setq target (or (assoc-default name project-mapping) name))
+      (if (eq (call-process "syncer2" () () () target) 0)
+          (message (concat "Synced " name " -> " target " and " (current-message)))
+        (message (concat (current-message) " - couldn't sync " name))))))
 
 (add-hook 'after-save-hook 'syncer)
