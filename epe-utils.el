679a9fd3c924095ccfced35fbed5a76bbf35983d
Eric Entzel <eric.entzel@rea-group.com>
2011-07-28 09:16:30 +1000

    Return the real original path rather than a placeholder when project not found (don't commit when tired...)

--- epe-utils.el
+++ epe-utils.el
@@ -4,18 +4,19 @@
   '("build.gradle" "Rakefile" "Gemfile" "Makefile" "pom.xml")
   "List of filenames that appear at project root")
 
-(defun guess-project-root (path)
+(defun guess-project-root (path &optional origpath)
   "Given a path, attempt to guess the project directory. Project
   directory is defined as the first enclosing directory which has
   at its root one of the files listed in project-root-files"
-  (let ((dir (file-name-directory path))
-        (result))
+  (let* ((dir (file-name-directory path))
+         (origpath (or origpath dir))
+         (result))
     (dolist (candidate project-root-files result)
       (if (file-exists-p (concat dir candidate))
           (setq result dir)))
     (cond (result)
-          ((string= dir "/") "original-path")
-          (t (guess-project-root (directory-file-name dir))))))
+          ((string= dir "/") origpath)
+          (t (guess-project-root (directory-file-name dir) origpath)))))
 
 (defun run-command-on-buffer-file (command)
   (shell-command (concat command " " buffer-file-name))
