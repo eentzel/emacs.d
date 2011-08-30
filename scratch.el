cad00a684f741615626efac2dddc32ca6026283b
Eric Entzel <eric.entzel@rea-group.com>
2011-08-31 09:29:29 +1000

    Err, should probably only prevent killing for the scratch buffer. The importance of negatvie testing...

--- scratch.el
+++ scratch.el
@@ -2,7 +2,8 @@
   "Bury scratch buffer and return nil. Intended to be added to
    kill-buffer-query-functions in order to prevent the scratch
    buffer being killed."
-  (bury-buffer)
-  nil)
+  (if (string= (buffer-name) "*scratch*")
+      (progn (bury-buffer) nil)
+    t))
 
 (add-to-list 'kill-buffer-query-functions 'bury-scratch)
