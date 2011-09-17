854d24fdf6f15da376e7e6748680dd1072c123c5
Eric Entzel <eric@ubermac.net>
2011-09-17 10:24:30 +1000

    Add graphviz dot mode

--- init.el
+++ init.el
@@ -102,6 +102,7 @@
 (if (file-exists-p "~/.emacs.d/pyflakes.el")
       (load "~/.emacs.d/pyflakes"))
 (load "~/.emacs.d/scratch.el")
+(load-file "~/.emacs.d/graphviz-dot-mode.el")
 
 ; automatically set these modes based on filenames:
 (add-to-list 'auto-mode-alist '("\\.ya?ml$" . yaml-mode))
