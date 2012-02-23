cc9f8c2d2ff25ade0194f6628752b6fcde491f7d
Eric Entzel <eric.entzel@rea-group.com>
2012-02-24 09:12:36 +1100

    Add js2-refactor as a submodule

--- js2-epe.el
+++ js2-epe.el
@@ -4,4 +4,8 @@
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
+(add-to-list 'load-path "~/.emacs.d/mark-multiple.el")
+(add-to-list 'load-path "~/.emacs.d/js2-refactor.el")
+(require 'js2-refactor)
+
 (load "~/.emacs.d/jslint-epe.el")
