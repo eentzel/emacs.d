225ad36a062d2053c234b8f4701d5913ac147826
Eric Entzel <eric.entzel@rea-group.com>
2012-06-08 13:31:51 +1000

    Turn off menubar when using emacs in a terminal (e.g., 'emacsclient -t')

--- init.el
+++ init.el
@@ -149,7 +149,7 @@
 
 ; turn off some annoying frills
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-; (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (setq ns-pop-up-frames nil)
 (transient-mark-mode nil)
 
