eccec395aa7183d8c4525276aa87efb2f0aeab73
Eric Entzel <eric@ubermac.net>
2013-06-01 21:50:38 +1000

    factor out some helpers

--- md-preview.el
+++ md-preview.el
@@ -3,6 +3,8 @@
 ;; elisp http: http://www.emacswiki.org/emacs/HttpPost
 
 (defun md-preview ()
+  "Uses the GitHub API to render the current buffer as GitHub
+Flavored Markdown and displays the results in a web browser"
   (interactive)
   (let ((endpoint                  "https://api.github.com/markdown/raw")
         (url-request-method        "POST")
@@ -10,16 +12,22 @@
         (url-request-data          (buffer-string)))
 
     (url-retrieve endpoint (lambda (status)
+                             (delete-http-headers)
                              (save-excursion
-                               ;; delete headers:
                                (goto-char 0)
-                               (push-mark)
-                               (forward-paragraph)
-                               (delete-region (point) (mark))
+                               (insert (gh-stylesheet-link)))
 
-                               ;; insert GitHub stylesheet
-                               (let ((stylesheet "https://gist.github.com/andyferra/2554919/raw/2e66cabdafe1c9a7f354aa2ebf5bc38265e638e5/github.css"))
-                                 (insert (format "<link rel='stylesheet' type='text/css' href='%s' />" stylesheet))))
-
-                             ;; open in browser:
                              (browse-url-of-buffer nil)))))
+
+
+(defun delete-http-headers ()
+  (save-excursion
+    (goto-char 0)
+    (push-mark)
+    (forward-paragraph)
+    (delete-region (point) (mark))))
+
+
+(defun gh-stylesheet-link ()
+  (let ((stylesheet "https://gist.github.com/andyferra/2554919/raw/2e66cabdafe1c9a7f354aa2ebf5bc38265e638e5/github.css"))
+    (format "<link rel='stylesheet' type='text/css' href='%s' />" stylesheet)))
