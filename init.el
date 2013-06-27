71de02aafe2185fe2a5cb9d1bb136d44822d746d
Eric Entzel <eric@ubermac.net>
2013-06-27 13:33:40 +1000

    Use login shell to grab path.
    OSX uses path_helper in /etc/profile to add some important entries.

--- init.el
+++ init.el
@@ -198,7 +198,7 @@
 ;; Fix path, which tends not to get set in OSX
 ;; From http://blog.gaz-jones.com/2012/02/01/setting_up_emacs_for_clojure_development.html
 (defun set-exec-path-from-shell-PATH ()
-  (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PATH'")))
+  (let ((path-from-shell (shell-command-to-string "$SHELL -l -c 'echo $PATH'")))
     (setenv "PATH" path-from-shell)
     (setq exec-path (split-string path-from-shell path-separator))))
 
