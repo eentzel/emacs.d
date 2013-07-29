cccd2c3a73b7a1bc3a3be1197f63fe8b524c67ad
Eric Entzel <eric@ubermac.net>
2013-07-29 14:52:12 +1000

    78 is a better default line length

--- init.el
+++ init.el
@@ -224,7 +224,7 @@
  '(c-basic-offset 4)
  '(desktop-save-mode t)
  '(ediff-split-window-function (quote split-window-horizontally))
- '(fill-column 160)
+ '(fill-column 78)
  '(hippie-expand-try-functions-list (quote (try-complete-file-name-partially try-complete-file-name try-expand-all-abbrevs try-expand-dabbrev try-expand-list try-expand-line try-expand-dabbrev-all-buffers try-expand-dabbrev-from-kill try-complete-lisp-symbol-partially try-complete-lisp-symbol)))
  '(ispell-program-name "aspell")
  '(js2-basic-offset 4)
