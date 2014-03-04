f404d3e740911a78b572fa1366b861520b2d1d0c
Eric Entzel <eric@ubermac.net>
2014-03-04 11:28:29 -0700

    Push prepending of "0x" up to hexify-region

--- hexify.el
+++ hexify.el
@@ -2,10 +2,10 @@
   (let* ((hex (mapcar (lambda (char)
                         (format "%X" char))
                       str)))
-    (reduce 'concat (cons "0x" hex))))
+    (apply 'concat hex)))
 
-(assert (string= "0x65726963" (hexify "eric")))
-(assert (string= "0x65656E747A656C" (hexify "eentzel")))
+(assert (string= "65726963" (hexify "eric")))
+(assert (string= "65656E747A656C" (hexify "eentzel")))
 
 (defun hexify-region ()
   "In transient-mark-mode, put the hexadecimal represntation of
@@ -15,7 +15,7 @@ region on the kill ring, prefixed with 0x"
     (let* ((start (region-beginning))
            (end   (region-end))
            (input (buffer-substring start end)))
-      (kill-new (hexify input))
+      (kill-new (concat "0x" (hexify input)))
       (setq deactivate-mark t))))
 
 (global-set-key "\C-ch" 'hexify-region)
