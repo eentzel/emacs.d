747539f5b499173dd3da6407f8091280400dc709
Eric Entzel <eric@ubermac.net>
2014-03-14 23:10:37 -0600

    Put backup files in their own directory

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(backup-directory-alist (quote (("." . "~/.emacs-backups"))))
  '(custom-safe-themes (quote ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
