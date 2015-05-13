8414769fbc67e045ac7775f7fdc6a8a1df02e857
Eric Entzel <eric@ubermac.net>
2015-05-13 14:19:12 -0600

    formatting

--- init.el
+++ init.el
@@ -222,8 +222,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ack-arguments (quote ("--color-lineno=bold yellow" "--color-filename=bold green" "--ignore-dir=classes" "--ignore-dir=build")))
- '(ack-project-root-file-patterns (quote (".project\\'" ".xcodeproj\\'" ".sln\\'" "\\`Project.ede\\'" "\\`.git\\'" "\\`.bzr\\'" "\\`_darcs\\'" "\\`.hg\\'" "pom.xml")))
+ '(ack-arguments
+   (quote
+    ("--color-lineno=bold yellow" "--color-filename=bold green" "--ignore-dir=classes" "--ignore-dir=build")))
+ '(ack-project-root-file-patterns
+   (quote
+    (".project\\'" ".xcodeproj\\'" ".sln\\'" "\\`Project.ede\\'" "\\`.git\\'" "\\`.bzr\\'" "\\`_darcs\\'" "\\`.hg\\'" "pom.xml")))
  '(backup-directory-alist (quote (("." . "~/.emacs-backups"))))
  '(c-basic-offset 4)
  '(cider-repl-history-file "~/.emacs.d/.cider-history")
